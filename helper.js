9d404cdc082838dffce6118f688e5d98e69ef4ee
colatoh <colatoh@users.noreply.github.com>
2015-04-08 10:09:42 -0600

    Update helper.js

--- helper.js
+++ helper.js
@@ -1,10 +1,30 @@
-'use strict';
+module.exports = {
+    quotaHelper: quotaHelper,
+    clientIp: clientIp,
+    cacheKey: cacheKey,
+    passwordCheck: passwordCheck,
+};
 
-        module.exports = {
-          passwordCheck: passwordCheck
-        };
+function quotaHelper(req) {
+    var key = req.swagger.params.name.value;
+    console.log('Quota Key: '+key)
+    return key;
+}
 
-        function passwordCheck(username, password, cb) {
-          var passwordOk = (username === 'scott' && password === 'apigee');
-          cb(null, passwordOk);
-        }
+function clientIp(req) {
+    var key = req.connection.remoteAddress;
+    console.log('clientIp Key: ' + key);
+    return key;
+}
+
+function cacheKey(req) {
+// This can check for a specific query parameter
+    var key = req.swagger.params.name.value;
+    console.log('Cache Key: '+key)
+    return key;
+}
+
+function passwordCheck(username, password, cb) {
+    var passwordOk = (username === 'cuuser' && password === 'gobuffs');
+    cb(null, passwordOk);
+}
